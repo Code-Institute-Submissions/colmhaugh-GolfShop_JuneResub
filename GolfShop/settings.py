f200158ad0c91df34aa6e72a0172bb648025e7e2
colmhaugh <colm.haugh@gmail.com>
2022-06-21 08:40:52 +0000

    Add add secret key

--- GolfShop/settings.py
+++ GolfShop/settings.py
@@ -22,12 +22,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-!4k6y13^xle83o7dcj1vgcahs@y26t)y$&+p)d&e(^lj-de02+'
+SECRET_KEY = 'django-insecure-!4k6y13^xle83o7dcj1vgcahs@y26t)y$&+p)d&e(^lj-de02+'
 
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+# SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+# DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = ['golfshopp5.herokuapp.com', 'localhost']
 

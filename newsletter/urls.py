7c3d8273818c6d6714ae2e68d4d0191070812f62
colmhaugh <colm.haugh@gmail.com>
2022-07-01 18:05:41 +0000

    Add newsletter models

--- newsletter/urls.py
+++ newsletter/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [
-    path('', views.newsletter, name='newsletter'),    
-]
+# urlpatterns = [
+#     path('', views.newsletter, name='newsletter'),    
+# ]
--- newsletter/views.py
+++ newsletter/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.urls import path
+from .models import Newsletter
+#from . import views
 
 # Create your views here.
+
+# urlpatterns = [
+#     path('', views.newsletter, name='newsletter')
+# ]
+
+# def get_subscribe(request):
+#     emails = Newsletter.objects.all()
+#     context = {
+#         'emails' : emails
+#     }
+#     return render(request, 'templates/newsletter.html', context)

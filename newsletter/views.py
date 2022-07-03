6bb39683f2adc20c257fd6f944ae50abbc7b0904
colmhaugh <colm.haugh@gmail.com>
2022-07-03 14:47:28 +0000

    Added fix for subscribe for newsletter

--- newsletter/views.py
+++ newsletter/views.py
@@ -8,7 +8,7 @@ from . import views
 def get_subscribe(request):
     if request.method == 'POST':
         email = request.POST.get('newsletter_email')
-        Newsletter.objects.create(email=email)
+        Newsletter.objects.create(email=email)        
 
         #return redirect(reverse('newsletter.html'))
         return render(request, 'newsletter.html')
